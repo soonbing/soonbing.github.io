99fe9ed37b987e9d197d4db27cf6c79fde5e8f6e
Soon Bing, GAN <soonbing@nus.edu.sg>
2013-09-04 21:06:32 +0800

    Changed fonts, fixed responsive design

--- override.js
+++ override.js
@@ -8,11 +8,16 @@
             Manually make sure that when the navigation link is clicked,
             the scroll will underscroll so that it isn't covered by the
             fixed elements on the top of the page
+            
+            http://stackoverflow.com/questions/14804941/how-to-add-smooth-scrolling-to-bootstraps-scroll-spy-function
+            
          */
-        $('.navbar li a').click(function(event) {
+        $(".navbar li a[href^='#']").click(function(event) {
             event.preventDefault();
-            $($(this).attr('href'))[0].scrollIntoView();
-            scrollBy(0, -offset);
+            // $($(this).attr('href'))[0].scrollIntoView();
+            // scrollBy(0, -offset);
+            $("html, body").animate({ scrollTop: $(this.hash).offset().top - offset}, 500);
+            
         });
     
     });
